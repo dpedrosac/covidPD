49f0d02b47f04e447d8fa93c02738ab701d56214
david <46906261+dpedrosac@users.noreply.github.com>
2021-09-30 23:34:09 +0200

    Update analyses.R
    modified format and added some structure to file

--- analyses.R
+++ analyses.R
@@ -1,4 +1,4 @@
-﻿# This is code to run analyses for the covidPD project;
+# This is code to run analyses for the covidPD project;
 # Code developed by Marlena van Munster, Marcel Printz and David Pedrosa
 
 ## First specify the packages of interest
@@ -16,7 +16,7 @@ package.check <- lapply(
 )
 
 ## In case of multiple people working on one project, this helps to create an automatic script
-
+# =================================================================================================
 username = Sys.info()["login"]
 ## @Marcel und Marlena, please insert your username here, so just enter Sys.info()["login"] in R and fill out username and add your folder at which you save the data below
 if (username == "dpedr") {
@@ -33,14 +33,17 @@ if (username == "dpedr") {
 setwd(wdir)
 
 # Load data and tidy up data
-df 				<- read.csv(file.path(wdir, "data", "raw_data.csv")) # read data frama from raw data
-cols			<- read.csv(file.path(wdir, "colnames.txt"), header=FALSE, stringsAsFactors=FALSE)$V1 # read column names
+# =================================================================================================
+df 		<- read.csv(file.path(wdir, "data", "raw_data.csv")) # read data frama from raw data
+cols		<- read.csv(file.path(wdir, "colnames.txt"), header=FALSE, stringsAsFactors=FALSE)$V1 # read column names
 colnames(df) 	<- cols[1:length(colnames(df))]								# replace column names with cleaned version in 'colnames.txt'
-values2replace = 93:99 # replaced everything that was not coded appropiatelya
+values2replace 	<- 93:99 # replaced everything that was not coded appropiatelya
+
 for (val in values2replace){ # replaces all values that make no sense with NA
 	df = df %>% mutate(df=replace(df, df==val, NA))
 }
 
 ## Create demographics for each country
+# =================================================================================================
 vars <- c("age.D1", "gender.D2", "educational_level.D8", "disease_duration.A1", "disease_stage.A1")
 tableOne <- CreateTableOne(vars=vars, data=df, strata="country") # @Marcel, here the vars of interest should be renamed and there should be a way to calculate medians instead of means if necessary (e.g., "A2_stageofPD") see also https://rpubs.com/kaz_yos/CreateTableOne1
